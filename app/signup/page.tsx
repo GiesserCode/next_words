d3483e7c0f45a8171b488b31fb133947d61a4d37
Flogie <floriangiessercode@outlook.com>
2023-12-15 21:54:04 +0100

    new commit

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import {cookies, headers} from "next/headers";
 import {createClient} from "@/utils/supabase/server";
-import {redirect} from "next/navigation";
+import "@/components/login.css";
 
 export default function Login({
                                   searchParams,
                               }: {
     searchParams: { message: string };
 }) {
-    const signIn = async (formData: FormData) => {
+    const signUp = async (formData: FormData) => {
         "use server";
 
         const origin = headers().get("origin");
@@ -26,15 +26,17 @@ export default function Login({
         });
 
         if (error) {
-            return redirect("/login?message=Could not authenticate user");
+            console.error("/login?message=Could not authenticate user");
         }
+
+        console.error("/login?message=Check email to continue sign in process");
     };
 
     return (
         <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
             <Link
                 href="/"
-                className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
+                className="focus:outline-none focus:bg-gblue absolute z-30 left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
             >
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -54,33 +56,52 @@ export default function Login({
                 Back
             </Link>
 
-            <form
-                className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-                action={signIn}
+            <div
+                className={
+                    "w-screen h-screen grid place-items-center absolute top-0 left-0"
+                }
             >
-                <label className="text-md" htmlFor="email">
-                    Email
-                </label>
-                <input
-                    className="rounded-md px-4 py-2 bg-inherit border mb-6"
-                    name="email"
-                    placeholder="you@example.com"
-                    required
-                />
-                <label className="text-md" htmlFor="password">
-                    Password
-                </label>
-                <input
-                    className="rounded-md px-4 py-2 bg-inherit border mb-6"
-                    type="password"
-                    name="password"
-                    placeholder="••••••••"
-                    required
-                />
-                <button className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2">
-                    Sign Up
-                </button>
-            </form>
+                <form
+                    className="flex flex-col border rounded-xl py-4 px-8 border-second"
+                    action={signUp}
+                >
+                    <h1 className={"w-full text-center text-3xl mb-2"}>Login</h1>
+                    <label className="text-sm text-second" htmlFor="name">
+                        Your Name
+                    </label>
+                    <input
+                        className="input"
+                        name="name"
+                        placeholder="John Doe"
+                        required
+                        autoComplete={"off"}
+                    />
+                    <label className="text-sm text-second" htmlFor="email">
+                        Email
+                    </label>
+                    <input
+                        className="input"
+                        name="email"
+                        placeholder="you@example.com"
+                        required
+                    />
+                    <label className="text-sm text-second" htmlFor="password">
+                        Password
+                    </label>
+                    <input
+                        className="input"
+                        type="password"
+                        name="password"
+                        placeholder="••••••••"
+                        required
+                    />
+                    <button className="button background-gradient">Sign Up</button>
+                    <p
+                        id="errorMessage"
+                        className="mt-2 p-2 bg-foreground/10 text-foreground text-red-500"
+                    ></p>
+                </form>
+            </div>
         </div>
     );
 }
